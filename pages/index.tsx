e35fbfcb69c9ae07d21e1f9efefd1be5bcb1603f
AbuBakkarSiddiqueAnir <anirdms@gamil.com>
2023-02-19 20:50:00 +0600

    getstaticprops used with demo data to test project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react"
+import { InferGetStaticPropsType } from "next"
+
+export async function getStaticProps(){
+  const products = [1,2,3]
+
+  return {
+    props:{
+      products
+    },
+    revalidate: 4*60*60
+  }
+}
 
 
-export default function Home() {
+export default function Home({products} : InferGetStaticPropsType<typeof getStaticProps>) {
 
-  const message: string = 'ehllow'
+useEffect(()=>{
+
+})
   return (
     <>
-    {message}
+      {products}
     </>
   )
 }
